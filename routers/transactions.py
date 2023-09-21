312ec40b2a2e87ffdfc85c9144a4b03d30ed4613
Afrifa518 <arffrifa@gmail.com>
2023-09-21 11:44:16 -0700

    Setup transfer system

--- routers/transactions.py
+++ routers/transactions.py
@@ -5,7 +5,7 @@ from sqlalchemy import desc, func
 sys.path.append("../..")
 
 from typing import Optional, List
-from fastapi import Depends, HTTPException, APIRouter
+from fastapi import Depends, HTTPException, APIRouter, status
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -421,3 +421,298 @@ async def mass_transaction_form(association_id: int,
         .all()
 
     return members, info
+
+
+class TransferSavings(BaseModel):
+    to_accountType: str
+    Amount: float
+    savings_acc_id: int
+    to_member_account_id: int
+
+
+@router.post("/transfer")
+async def transfers_in_savings_account(tranfer: TransferSavings,
+                                       user: dict = Depends(get_current_user),
+                                       db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    if tranfer.to_accountType == "savings":
+        to_acc = db.query(models.Members.firstname,
+                          models.Members.lastname,
+                          models.SavingsAccount.account_name, ) \
+            .select_from(models.MemberSavingsAccount) \
+            .join(models.Members, models.MemberSavingsAccount.member_id == models.Members.member_id) \
+            .join(models.SavingsAccount, models.SavingsAccount.id == models.MemberSavingsAccount.savings_id) \
+            .filter(models.MemberSavingsAccount.id == tranfer.to_member_account_id) \
+            .first()
+
+        from_acc = db.query(models.Members.firstname,
+                            models.Members.lastname,
+                            models.SavingsAccount.account_name,
+                            models.MemberSavingsAccount.current_balance) \
+            .select_from(models.MemberSavingsAccount) \
+            .join(models.Members, models.MemberSavingsAccount.member_id == models.Members.member_id) \
+            .join(models.SavingsAccount, models.SavingsAccount.id == models.MemberSavingsAccount.savings_id) \
+            .filter(models.MemberSavingsAccount.id == tranfer.savings_acc_id) \
+            .first()
+
+        if tranfer.Amount <= from_acc.current_balance:
+            transfer_from = models.SavingsTransaction()
+            transfer_from.amount = tranfer.Amount
+            transfer_from.narration = f"Transferred to: {to_acc.firstname} {to_acc.lastname}'s {to_acc.account_name} account"
+            transfer_from.savings_acc_id = tranfer.savings_acc_id
+            transfer_from.transactiontype_id = 2
+            transfer_from.transaction_date = datetime.now()
+            transfer_from.prep_by = user.get("id")
+            db.add(transfer_from)
+            db.commit()
+
+            trn = models.SavingsTransaction()
+            trn.transactiontype_id = 1
+            trn.amount = tranfer.Amount
+            trn.narration = f"Received from: {from_acc.firstname} {from_acc.lastname}'s {from_acc.account_name} account"
+            trn.savings_acc_id = tranfer.to_member_account_id
+            trn.prep_by = user.get("id")
+            trn.transaction_date = datetime.now()
+            db.add(trn)
+            db.commit()
+
+            return "Transfer Complete"
+        else:
+            raise insufficient_balance()
+
+    elif tranfer.to_accountType == "loans":
+        to_acc = db.query(models.Members.firstname,
+                          models.Members.lastname,
+                          models.LoanAccount.account_name) \
+            .select_from(models.MemberLoanAccount) \
+            .join(models.Members, models.MemberLoanAccount.member_id == models.Members.member_id) \
+            .join(models.LoanAccount, models.LoanAccount.id == models.MemberLoanAccount.loan_id) \
+            .filter(models.MemberLoanAccount.id == tranfer.to_member_account_id) \
+            .first()
+
+        from_acc = db.query(models.Members.firstname,
+                            models.Members.lastname,
+                            models.SavingsAccount.account_name,
+                            models.MemberSavingsAccount.current_balance) \
+            .select_from(models.MemberSavingsAccount) \
+            .join(models.Members, models.MemberSavingsAccount.member_id == models.Members.member_id) \
+            .join(models.SavingsAccount, models.SavingsAccount.id == models.MemberSavingsAccount.savings_id) \
+            .filter(models.MemberSavingsAccount.id == tranfer.savings_acc_id) \
+            .first()
+
+        if tranfer.Amount <= from_acc.current_balance:
+            transfer_from = models.SavingsTransaction()
+            transfer_from.amount = tranfer.Amount
+            transfer_from.narration = f"Transferred to: {to_acc.firstname} {to_acc.lastname}'s {to_acc.account_name} account"
+            transfer_from.savings_acc_id = tranfer.savings_acc_id
+            transfer_from.transactiontype_id = 2
+            transfer_from.transaction_date = datetime.now()
+            transfer_from.prep_by = user.get("id")
+            db.add(transfer_from)
+            db.commit()
+
+            trn_to = models.LoansTransaction()
+            trn_to.transactiontype_id = 1
+            trn_to.amount = tranfer.Amount
+            trn_to.narration = f"Paid from: {from_acc.firstname} {from_acc.lastname}'s {from_acc.account_name} account"
+            trn_to.loans_acc_id = tranfer.to_member_account_id
+            trn_to.prep_by = user.get("id")
+            trn_to.transaction_date = datetime.now()
+            trn_to.status = "Pay off"
+            db.add(trn_to)
+            db.commit()
+            return "Transfer Complete"
+        else:
+            raise insufficient_balance()
+
+    elif tranfer.to_accountType == "shares":
+        to_acc = db.query(models.Members.firstname,
+                          models.Members.lastname,
+                          models.ShareAccount.account_name) \
+            .select_from(models.MemberShareAccount) \
+            .join(models.Members, models.MemberShareAccount.member_id == models.Members.member_id) \
+            .join(models.ShareAccount, models.ShareAccount.id == models.MemberShareAccount.share_id) \
+            .filter(models.MemberShareAccount.id == tranfer.to_member_account_id) \
+            .first()
+
+        from_acc = db.query(models.Members.firstname,
+                            models.Members.lastname,
+                            models.SavingsAccount.account_name,
+                            models.MemberSavingsAccount.current_balance) \
+            .select_from(models.MemberSavingsAccount) \
+            .join(models.Members, models.MemberSavingsAccount.member_id == models.Members.member_id) \
+            .join(models.SavingsAccount, models.SavingsAccount.id == models.MemberSavingsAccount.savings_id) \
+            .filter(models.MemberSavingsAccount.id == tranfer.savings_acc_id) \
+            .first()
+
+    if tranfer.Amount <= from_acc.current_balance:
+        transfer_from = models.SavingsTransaction()
+        transfer_from.amount = tranfer.Amount
+        transfer_from.narration = f"Transferred to: {to_acc.firstname} {to_acc.lastname}'s {to_acc.account_name} account"
+        transfer_from.savings_acc_id = tranfer.savings_acc_id
+        transfer_from.transactiontype_id = 2
+        transfer_from.transaction_date = datetime.now()
+        transfer_from.prep_by = user.get("id")
+        db.add(transfer_from)
+        db.commit()
+
+        trn_to = models.SharesTransaction()
+        trn_to.transactiontype_id = 1
+        trn_to.amount = tranfer.Amount
+        trn_to.narration = f"Purchased from: {from_acc.firstname} {from_acc.lastname}'s {from_acc.account_name} account"
+        trn_to.shares_acc_id = tranfer.to_member_account_id
+        trn_to.prep_by = user.get("id")
+        trn_to.transaction_date = datetime.now()
+        db.add(trn_to)
+        db.commit()
+        return f"Transfer Complete"
+    else:
+        raise insufficient_balance()
+
+
+@router.get("transfer/info/{member_id}")
+async def get_info_for_transfer(member_id,
+                                user: dict = Depends(get_current_user),
+                                db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    # Savings Accounts
+    all_savings = db.query(models.MemberSavingsAccount.id,
+                           models.Members.firstname,
+                           models.Members.lastname,
+                           models.Association.association_name,
+                           models.SavingsAccount.account_name) \
+        .select_from(models.MemberSavingsAccount) \
+        .join(models.Members, models.Members.member_id == models.MemberSavingsAccount.member_id) \
+        .join(models.SavingsAccount, models.SavingsAccount.id == models.MemberSavingsAccount.savings_id) \
+        .join(models.AssociationMembers,
+              models.AssociationMembers.association_members_id == models.MemberSavingsAccount.association_member_id) \
+        .join(models.Association, models.Association.association_id == models.AssociationMembers.association_id) \
+        .filter(models.MemberSavingsAccount.member_id != member_id) \
+        .all()
+
+    individual_accounts = db.query(models.MemberSavingsAccount.id,
+                                   models.Members.firstname,
+                                   models.Members.lastname,
+                                   models.SavingsAccount.account_name) \
+        .select_from(models.MemberSavingsAccount) \
+        .join(models.Members, models.Members.member_id == models.MemberSavingsAccount.member_id) \
+        .join(models.SavingsAccount, models.SavingsAccount.id == models.MemberSavingsAccount.savings_id) \
+        .outerjoin(models.AssociationMembers, models.Members.member_id == models.AssociationMembers.members_id) \
+        .filter(models.AssociationMembers.members_id.is_(None)) \
+        .filter(models.MemberSavingsAccount.member_id != member_id) \
+        .all()
+
+    my_savings = db.query(models.MemberSavingsAccount.id,
+                          models.Association.association_name,
+                          models.SavingsAccount.account_name) \
+        .select_from(models.MemberSavingsAccount) \
+        .join(models.Members, models.Members.member_id == models.MemberSavingsAccount.member_id) \
+        .join(models.SavingsAccount, models.SavingsAccount.id == models.MemberSavingsAccount.savings_id) \
+        .join(models.AssociationMembers,
+              models.AssociationMembers.association_members_id == models.MemberSavingsAccount.association_member_id) \
+        .join(models.Association, models.Association.association_id == models.AssociationMembers.association_id) \
+        .filter(models.MemberSavingsAccount.member_id == member_id) \
+        .all()
+
+    # Loans Accounts
+
+    all_loans = db.query(models.MemberLoanAccount.id,
+                         models.Members.firstname,
+                         models.Members.lastname,
+                         models.Association.association_name,
+                         models.LoanAccount.account_name) \
+        .select_from(models.MemberLoanAccount) \
+        .join(models.Members, models.Members.member_id == models.MemberLoanAccount.member_id) \
+        .join(models.LoanAccount, models.LoanAccount.id == models.MemberLoanAccount.loan_id) \
+        .join(models.AssociationMembers,
+              models.AssociationMembers.association_members_id == models.MemberLoanAccount.association_member_id) \
+        .join(models.Association, models.Association.association_id == models.AssociationMembers.association_id) \
+        .filter(models.MemberLoanAccount.member_id != member_id) \
+        .all()
+
+    individual_loan_accounts = db.query(models.MemberLoanAccount.id,
+                                        models.Members.firstname,
+                                        models.Members.lastname,
+                                        models.LoanAccount.account_name) \
+        .select_from(models.MemberLoanAccount) \
+        .join(models.Members, models.Members.member_id == models.MemberLoanAccount.member_id) \
+        .join(models.LoanAccount, models.LoanAccount.id == models.MemberLoanAccount.loan_id) \
+        .outerjoin(models.AssociationMembers, models.Members.member_id == models.AssociationMembers.members_id) \
+        .filter(models.AssociationMembers.members_id.is_(None)) \
+        .filter(models.MemberLoanAccount.member_id != member_id) \
+        .all()
+
+    my_loans = db.query(models.MemberLoanAccount.id,
+                        models.Association.association_name,
+                        models.LoanAccount.account_name) \
+        .select_from(models.MemberLoanAccount) \
+        .join(models.Members, models.Members.member_id == models.MemberLoanAccount.member_id) \
+        .join(models.LoanAccount, models.LoanAccount.id == models.MemberLoanAccount.loan_id) \
+        .join(models.AssociationMembers,
+              models.AssociationMembers.association_members_id == models.MemberLoanAccount.association_member_id) \
+        .join(models.Association, models.Association.association_id == models.AssociationMembers.association_id) \
+        .filter(models.MemberLoanAccount.member_id == member_id) \
+        .all()
+
+    # Shares Accounts
+
+    all_shares = db.query(models.MemberShareAccount.id,
+                          models.Members.firstname,
+                          models.Members.lastname,
+                          models.Association.association_name,
+                          models.ShareAccount.account_name) \
+        .select_from(models.MemberShareAccount) \
+        .join(models.Members, models.Members.member_id == models.MemberShareAccount.member_id) \
+        .join(models.ShareAccount, models.ShareAccount.id == models.MemberShareAccount.share_id) \
+        .join(models.AssociationMembers,
+              models.AssociationMembers.association_members_id == models.MemberShareAccount.association_member_id) \
+        .join(models.Association, models.Association.association_id == models.AssociationMembers.association_id) \
+        .filter(models.MemberShareAccount.member_id != member_id) \
+        .all()
+
+    individual_share_accounts = db.query(models.MemberShareAccount.id,
+                                         models.Members.firstname,
+                                         models.Members.lastname,
+                                         models.ShareAccount.account_name) \
+        .select_from(models.MemberShareAccount) \
+        .join(models.Members, models.Members.member_id == models.MemberShareAccount.member_id) \
+        .join(models.ShareAccount, models.ShareAccount.id == models.MemberShareAccount.share_id) \
+        .outerjoin(models.AssociationMembers, models.Members.member_id == models.AssociationMembers.members_id) \
+        .filter(models.AssociationMembers.members_id.is_(None)) \
+        .filter(models.MemberShareAccount.member_id != member_id) \
+        .all()
+
+    my_shares = db.query(models.MemberShareAccount.id,
+                         models.Association.association_name,
+                         models.ShareAccount.account_name) \
+        .select_from(models.MemberShareAccount) \
+        .join(models.Members, models.Members.member_id == models.MemberShareAccount.member_id) \
+        .join(models.ShareAccount, models.ShareAccount.id == models.MemberShareAccount.share_id) \
+        .join(models.AssociationMembers,
+              models.AssociationMembers.association_members_id == models.MemberShareAccount.association_member_id) \
+        .join(models.Association, models.Association.association_id == models.AssociationMembers.association_id) \
+        .filter(models.MemberShareAccount.member_id == member_id) \
+        .all()
+
+    return {
+        "All_Savings_Acc": all_savings,
+        "My_Savings_Acc": my_savings,
+        "Individual_Acc": individual_accounts,
+        "All_Loans_Acc": all_loans,
+        "My_Loans_Acc": my_loans,
+        "Individual_Loans_Acc": individual_loan_accounts,
+        "All_Shares_Acc": all_shares,
+        "My_Shares_Acc": my_shares,
+        "Individual_Shares_Acc": individual_share_accounts
+    }
+
+
+def insufficient_balance():
+    error_nkoa = HTTPException(
+        status_code=status.HTTP_417_EXPECTATION_FAILED,
+        detail="Insufficient Balance to perform transfer",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return error_nkoa
