3f5a3d1580f9f771326186b035302be3b008275e
Afrifa518 <117725028+Afrifa518@users.noreply.github.com>
2023-10-15 07:43:23 -1200

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/FinanceDept"
+SQLALCHEMY_DATABASE_URL = "postgres://cooperative_database_user:jTQNnH8XtJNTOmegy09q6e2W6sHUF8Zv@dpg-ckm3nfo710pc73falujg-a/cooperative_database"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
