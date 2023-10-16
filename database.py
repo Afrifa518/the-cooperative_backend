6ac191a065d491205a38bef909b866ffd9bdb158
Afrifa518 <arffrifa@gmail.com>
2023-10-15 20:00:48 -1200

    Made some corrections again yes

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/FinanceDept"
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
 
+# postgresql://cooperative_database_user:jTQNnH8XtJNTOmegy09q6e2W6sHUF8Zv@dpg-ckm3nfo710pc73falujg-a.oregon-postgres.render.com/cooperative_database
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
